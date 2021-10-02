0b690e929381c1a6536157802059291a6252ffd0
saifulislammunna <saifulmunna28@gmail.com>
2021-10-02 19:02:09 +0600

    familiar-rendering-added

---
+++ src/components/Player/Player.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Player = () => {
+    return (
+        <div>
+          <p>A player</p>  
+        </div>
+    );
+};
+
+export default Player;
---
+++ src/components/Teacher/Teacher.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Teacher = () => {
+    return (
+        <div>
+            <p>A teacher</p>
+        </div>
+    );
+};
+
+export default Teacher;
